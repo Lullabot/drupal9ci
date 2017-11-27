c53cbf3b76f8644ab0d783e17cf0a67229b0ce9c
Juampy NR <juampynr@users.noreply.github.com>
2017-11-27 13:55:52 +0100

    Fix rsync pattern in setup.sh

--- setup.sh
+++ setup.sh
@@ -2,5 +2,5 @@
 
 wget https://github.com/juampynr/drupal8ci/archive/master.zip
 unzip master.zip 'drupal8ci-master/dist/*'
-rsync -vaz drupal8ci-master/dist/* .
+rsync -vaz drupal8ci-master/dist/ .
 rm master.zip
