1c2f78c1fe4498aa4cf0b20fbf6439d9e2d229ab
Juampy NR <juampynr@users.noreply.github.com>
2017-11-27 13:48:43 +0100

    Fix again the zip filename

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 wget https://github.com/juampynr/drupal8ci/archive/master.zip
-unzip master.zip 'drupal8ci/dist/*'
+unzip drupal8ci-master.zip 'drupal8ci/dist/*'
 rsync -vaz drupal8ci/dist/* .
-rm master.zip
+rm drupal8ci-master.zip
