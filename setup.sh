be156cbcb4f5614fbd1e81f900d9ffa8cafba9fc
Juampy NR <juampynr@users.noreply.github.com>
2017-11-27 13:51:26 +0100

    Fix (yes, again) the setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 wget https://github.com/juampynr/drupal8ci/archive/master.zip
-unzip drupal8ci-master.zip 'drupal8ci/dist/*'
-rsync -vaz drupal8ci/dist/* .
-rm drupal8ci-master.zip
+unzip master.zip 'drupal8ci-master/dist/*'
+rsync -vaz drupal8ci-master/dist/* .
+rm master.zip
