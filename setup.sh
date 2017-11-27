ea5f4069a6b88bf62335375e155639ef640eb2a6
Juampy NR <juampynr@users.noreply.github.com>
2017-11-27 13:46:20 +0100

    Fix zip filename in setup script

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 wget https://github.com/juampynr/drupal8ci/archive/master.zip
-unzip drupal8ci.zip 'drupal8ci/dist/*'
+unzip master.zip 'drupal8ci/dist/*'
 rsync -vaz drupal8ci/dist/* .
-rm drupal8ci.zip
+rm master.zip
