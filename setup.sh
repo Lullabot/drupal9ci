5ec5d8bfaecb29151a4781fbe5eb90236d488174
Juampy <juampynr@gmail.com>
2017-12-18 18:24:30 +0100

    Add composer dependencies in installer

--- setup.sh
+++ setup.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
+# Download and extract CircleCI configuration and sample tests.
 wget https://github.com/juampynr/drupal8ci/archive/master.zip
 unzip master.zip 'drupal8ci-master/dist/*'
 rsync -vaz drupal8ci-master/dist/ .
 rm master.zip
 rm -rf drupal8ci-master
+
+# Add development dependencies to run the CircleCI jobs.
+#
+# behat/mink-extension is pinned until https://github.com/Behat/MinkExtension/pull/311 gets fixed.
+composer require --dev --no-update \
+    cweagans/composer-patches \
+    behat/mink-extension:v2.2 \
+    behat/mink-selenium2-driver:^1.3 \
+    bex/behat-screenshot \
+    drupal/coder:^8.2 \
+    drupal/drupal-extension:master-dev \
+    drush/drush:~8.1 \
+    guzzlehttp/guzzle:^6.0@dev
