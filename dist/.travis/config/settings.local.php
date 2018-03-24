73c29a6e417db7a7acd5f407c37dae15fad3a04f
Juampy <juampynr@gmail.com>
2018-03-24 17:49:52 +0100

    Run behat tests in Travis CI
    * Uses docker4drupal to build the environment.
    * Uses Travis CI's test matrix to run jobs in parallel.
    * Adds an installer, which is the first step in reorganizing the
      CI integrations in this repository so each one has it's own directory.

---
+++ dist/.travis/config/settings.local.php
@@ -0,0 +1,16 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => 'mariadb',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
+
+$settings['install_profile'] = 'standard';
+$config_directories['sync'] = 'sites/default/files/config_KaEbND6z9WvIBDnApD_YmTrI42ZryedG2lAUC30RFvx8I_7eU9TcvwMhFk8d-_GO-Snp9y5YwA/sync';
+$settings['hash_salt'] = 'MuobC_x0x3Cmw9gG6X366aMTR0CF_P8a4iK4rz4iKoLHYU7pwEDSldx5YdX6tDDBJcr1JICqrw';
