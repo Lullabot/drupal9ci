755667c7fb1bb603f7aef80731a4f72c02be512d
Juampy <juampynr@gmail.com>
2018-03-25 13:04:16 +0200

    [CircleCI] Import database without installing it first for Behat tests
    This approach uses a settings.local.php file, which gives a chance
    to add further overrides if needed.

---
+++ dist/circleci/.circleci/config/settings.local.php
@@ -0,0 +1,12 @@
+<?php
+
+$databases['default']['default'] = array (
+  'database' => 'drupal8',
+  'username' => 'root',
+  'password' => '',
+  'prefix' => '',
+  'host' => '127.0.0.1',
+  'port' => '',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+);
