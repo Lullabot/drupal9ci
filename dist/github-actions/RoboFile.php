5ea8d73c946a697bb37ad5159400c7c999a012ec
Fran Garcia-Linares <fjgarlin@gmail.com>
2021-04-11 17:39:21 +0200

    Replace web folder path apache.

--- dist/github-actions/RoboFile.php
+++ dist/github-actions/RoboFile.php
@@ -169,6 +169,8 @@ class RoboFile extends \Robo\Tasks {
     $tasks = [];
     $tasks[] = $this->taskFilesystemStack()
       ->copy('.github/config/behat.yml', 'tests/behat.yml', $force);
+    $tasks[] = $this->taskExec('sed -ri -e \'s!/var/www/html!' . getenv('GITHUB_WORKSPACE') . '/web!g\' /etc/apache2/sites-available/*.conf /etc/apache2/apache2.conf /etc/apache2/conf-available/*.conf');
+    $tasks[] = $this->taskExec('service apache2 start');
     $tasks[] = $this->taskExec('vendor/bin/behat --verbose -c tests/behat.yml');
     return $tasks;
   }
