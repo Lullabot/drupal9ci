89979b274e6fd3c7edf1966a8cd98dee04454218
Fran Garcia-Linares <fjgarlin@gmail.com>
2021-04-14 13:38:47 +0200

    Random username and email to run the test multiple times.

--- dist/common/tests/cypress/user_register.js
+++ dist/common/tests/cypress/user_register.js
@@ -1,11 +1,13 @@
 describe('Register page', () => {
   it('Registers a new user', () => {
+    const id = Date.now().toString()
+
     cy.visit('/')
     cy.contains('Log in').click()
     cy.contains('Create new account').click()
     cy.url().should('include', '/register')
-    cy.get('input[name=name]').type('John Doe')
-    cy.get('input[name=mail]').type('john.doe@domain.com')
+    cy.get('input[name=name]').type(`john_doe_${id}`)
+    cy.get('input[name=mail]').type(`john_doe_${id}@domain.com`)
     cy.get('#edit-submit').click()
     cy.contains('Thank you').should('be.visible')
   })
