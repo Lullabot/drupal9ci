03739f83bdf76acc2200c5eb51d4b30759f546ef
Fran Garcia-Linares <fjgarlin@gmail.com>
2021-04-14 10:40:25 +0200

    Change integration folder settings.

--- dist/circleci/.circleci/RoboFile.php
+++ dist/circleci/.circleci/RoboFile.php
@@ -172,7 +172,7 @@ class RoboFile extends \Robo\Tasks
             ->copy('.cypress/package.json', 'package.json', $force);
         $tasks[] = $this->taskExec('sleep 30s');
         $tasks[] = $this->taskExec('npm install cypress --save-dev');
-        $tasks[] = $this->taskExec('$(npm bin)/cypress run --spec "./tests/cypress/*.js"');
+        $tasks[] = $this->taskExec('$(npm bin)/cypress run');
         return $tasks;
     }
 
