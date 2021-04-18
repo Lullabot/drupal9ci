c8ecfd02172a8210d623ada0e981408f7bdebc7d
Fran Garcia-Linares <fjgarlin@gmail.com>
2021-04-18 11:50:57 +0200

    Simplified user register test.

--- dist/common/tests/cypress/user_register.js
+++ dist/common/tests/cypress/user_register.js
@@ -2,10 +2,7 @@ describe('Register page', () => {
   it('Registers a new user', () => {
     const id = Date.now().toString()
 
-    cy.visit('/')
-    cy.contains('Log in').click()
-    cy.contains('Create new account').click()
-    cy.url().should('include', '/register')
+    cy.visit('/user/register')
     cy.get('input[name=name]').type(`john_doe_${id}`)
     cy.get('input[name=mail]').type(`john_doe_${id}@domain.com`)
     cy.get('#edit-submit').click()
